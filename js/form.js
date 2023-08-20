4b757f38d08d75f4c6d25f2b4df5712944e1ab1d
Panyatna <TimurKabir@yandex.ru>
2023-08-20 09:42:23 +0500

    добавил неактивное состояние форм

---
+++ js/form.js
@@ -0,0 +1,34 @@
+const formNode = document.querySelector('.ad-form');
+const formFieldsetNodes = formNode.querySelectorAll('fieldset');
+
+const mapNode = document.querySelector('.map__filters');
+const mapSelectNodes = mapNode.querySelectorAll('.map__filter');
+const mapFieldsetNodes = mapNode.querySelector('.map__features');
+
+const disableForm = () => {
+  formNode.classList.add('.ad-form--disabled');
+  formFieldsetNodes.forEach((fieldsetNode) => {
+    fieldsetNode.setAttribute('disabled', '');
+  });
+
+  mapNode.classList.add('.ad-form--disabled');
+  mapFieldsetNodes.setAttribute('disabled', '');
+  mapSelectNodes.forEach((mapNode) => {
+    mapNode.setAttribute('disabled', '');
+  });
+};
+
+const enableForm = () => {
+  formNode.classList.remove('.ad-form--disabled');
+  formFieldsetNodes.forEach((fieldsetNode) => {
+    fieldsetNode.removeAttribute('disabled', '');
+  });
+
+  mapNode.classList.remove('.ad-form--disabled');
+  mapFieldsetNodes.removeAttribute('disabled', '');
+  mapSelectNodes.forEach((mapNode) => {
+    mapNode.removeAttribute('disabled', '');
+  });
+};
+
+export {disableForm, enableForm}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import './render.js'
+import {disableForm, enableForm} from './form.js';
 
-
-
+disableForm();
+enableForm();
