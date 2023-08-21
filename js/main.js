3a8f00c3cd0063fe58ddf2d691fed2245c662900
Panyatna <TimurKabir@yandex.ru>
2023-08-21 20:13:18 +0500

    добавил часть валидации

--- js/main.js
+++ js/main.js
@@ -1,5 +1,50 @@
 import './render.js'
 import {disableForm, enableForm} from './form.js';
 
+const form = document.querySelector('.ad-form');
+
+const pristine = new Pristine(form, {
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form__element--invalid',
+  errorTextParent: 'ad-form__element',
+  errorTextClass: 'text-help',
+});
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  const isValid = pristine.validate();
+  if (isValid) {
+    console.log('Можно отправлять');
+  } else {
+    console.log('Форма невалидна');
+  }
+});
+
+const pors = () => {
+  const find = document.querySelector('#type').value;
+  const lind = document.querySelector('#price');
+  const ert = document.querySelector('#type').onclick = () => {
+    if (find === 'flat') {
+      lind.placeholder = 1600
+    } if (find === 'bungalow') {
+      lind.placeholder = 1700
+      } if (find === 'hotel') {
+        lind.placeholder = 1800
+        } if (find === 'house') {
+          lind.placeholder = 1900
+          } if (find === 'palace') {
+            lind.placeholder = 2000
+            }
+  };
+};
+
+// const ert = document.querySelector('#type').onclick = () => {
+//   console.log("Вы нажали на кнопку");
+// }
+
+
+pors();
+
 disableForm();
 enableForm();
